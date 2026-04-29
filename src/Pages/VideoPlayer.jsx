0ec9741059df8ca1bc02e44331919313c0449a5a
hds05 <duttsharmahimanshu96@gmail.com>
2026-04-29 22:36:41 +0530

    Add axios dependency and enhance VideoPlayer component with video details, likes, dislikes, subscribers, etc. interaction buttons

--- src/Pages/VideoPlayer.jsx
+++ src/Pages/VideoPlayer.jsx
@@ -21,13 +21,48 @@ function VideoPlayer() {
               allow='autoplay'
             ></iframe>
           </div>
+
+          <div className="mt-4">
+            <h1 className="text-lg md:text-xl font-semibold">
+              {filtered?.title}
+            </h1>
+
+            <div className="flex flex-col md:flex-row md:items-center md:justify-between mt-3 gap-4">
+              <div className="flex items-center gap-3">
+                <img
+                  src={filtered?.channelIcon}
+                  alt="channel"
+                  className="w-10 h-10 rounded-full"
+                />
+                <div>
+                  <p className="font-medium">{filtered?.channelId}</p>
+                  <p className="text-sm text-gray-400">{filtered?.subscribers}</p>
+                </div>
+                <button className="ml-4 bg-white text-black px-4 py-1 rounded-full font-medium cursor-pointer">
+                  Subscribe
+                </button>
+              </div>
+
+              <div className="flex gap-3">
+                <button className="bg-gray-800 px-3 py-1 rounded-full cursor-pointer">👍 <span className='border-l-1 pl-2'> {filtered.likes}</span></button>
+                <button className="bg-gray-800 px-3 py-1 rounded-full cursor-pointer">👎 <span className='border-l-1 pl-2'> {filtered.dislikes}</span></button>
+                <button className="bg-gray-800 px-3 py-1 rounded-full cursor-pointer">🔗 Share</button>
+              </div>
+            </div>
+
+            <div className="bg-gray-900 p-3 rounded-lg mt-4 text-sm text-gray-300">
+              <p>
+                {filtered.description}
+              </p>
+            </div>
+          </div>
         </div>
 
         <div>
           {/* make comment section here */}
         </div>
 
-        <div className="w-full lg:w-[350px] flex flex-col gap-4">
+        <div className="w-full lg:w-[350px] bg-black/40 p-2 rounded-2xl flex flex-col gap-4">
           {videos.map((item) => (
             <div key={item.videoId} onClick={() => navigate(`/video/${item.videoId}`)} className="flex gap-3 cursor-pointer">
               <img src={item.thumbnailUrl} className="w-40 h-24 bg-gray-800 rounded-lg" />
