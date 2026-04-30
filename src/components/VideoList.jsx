754de8609f23adbdabf6ab6699cba2e41adee679
hds05 <duttsharmahimanshu96@gmail.com>
2026-05-01 01:04:00 +0530

    Fetch videos from API and implement search filtering in VideoList component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import VideoCard from './VideoCard'
-import { videos } from '../assets/dummydata'
+import axios from 'axios';
+import { useSearch } from '../../context/SearchContext';
 
 function VideoList({ category }) {
+    const [videos, setVideos] = useState([])
+    const { searchText } = useSearch()
 
-    const filteredVideos = category === "All" ? videos : videos.filter((e) => e.category?.toLowerCase() === category.toLowerCase())
+    useEffect(() => {
+        axios.get("http://localhost:3000/videos")
+            .then(res => setVideos(res.data))
+            .catch(err => console.log(err))
+    }, []);
+    const filteredVideos = videos.filter((video) => {
+        const matchesCategory =
+            category === "All" ||
+            video.category?.toLowerCase() === category.toLowerCase()
+
+        const matchesSearch =
+            video.title?.toLowerCase().includes(searchText.toLowerCase())
+
+        return matchesCategory && matchesSearch
+    })
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4'>
             {
