9bf990b4488ef7412334bda93bd21bbf7d55dc57
hds05 <duttsharmahimanshu96@gmail.com>
2026-05-02 10:29:05 +0530

    update ProfileMenu component with icons for better user experience

--- src/components/ProfileMenu.jsx
+++ src/components/ProfileMenu.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
+import { MdOutlineSwitchAccount } from "react-icons/md";
+import { FaSignOutAlt } from "react-icons/fa";
+import { SiYoutubestudio } from "react-icons/si";
+import { CiDollar } from "react-icons/ci";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { MdNightlightRound } from "react-icons/md";
+import { IoLanguage } from "react-icons/io5";
+import { RiUserSettingsLine } from "react-icons/ri";
+import { TbCurrentLocationFilled } from "react-icons/tb";
+import { FaKeyboard } from "react-icons/fa";
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdOutlineHelpOutline } from "react-icons/md";
+import { VscFeedback } from "react-icons/vsc";
+
 
 export default function ProfileMenu({
     open,
@@ -33,60 +48,60 @@ export default function ProfileMenu({
                 <div className="my-2 mx-2 border-t"></div>
 
                 <div className="py-2 custom-scrollbar overflow-auto max-h-80">
-                    <div className="w-full cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
-                        Google Account
+                    <div className="w-full flex items-center gap-2 cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
+                        <FaGoogle /> Google Account
                     </div>
-                    <div className="w-full cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
-                        Switch Account
+                    <div className="w-full flex items-center gap-2 cursor-pointer cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
+                        <MdOutlineSwitchAccount /> Switch Account
                     </div>
                     <button
                         onClick={logout}
-                        className="w-full cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900 hover:text-red-700"
+                        className="w-full flex items-center gap-2 cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900 hover:text-red-700"
                     >
-                        Sign Out
+                        <FaSignOutAlt />  Sign Out
                     </button>
 
                     <div className="my-2 mx-2 border-t"></div>
-                    
-                    <div className="w-full cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
-                        YouTube Studio
+
+                    <div className="w-full flex items-center gap-2 cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
+                        <SiYoutubestudio /> YouTube Studio
                     </div>
-                    <div className="w-full cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
-                        Purchase & memberships
+                    <div className="w-full flex items-center gap-2 cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
+                        <CiDollar /> Purchase & memberships
                     </div>
 
                     <div className="my-2 mx-2 border-t"></div>
-                    
-                    <div className="w-full cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
-                        Your data in YouTubee
+
+                    <div className="w-full flex items-center gap-2 cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
+                        <FaRegCircleUser /> Your data in YouTubee
                     </div>
-                    <div className="w-full cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
-                        Appereance: Device Theme
+                    <div className="w-full flex items-center gap-2 cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
+                        <MdNightlightRound /> Appereance: Device Theme
                     </div>
-                    <div className="w-full cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
-                        Display language: English
+                    <div className="w-full flex items-center gap-2 cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
+                        <IoLanguage /> Display language: English
                     </div>
-                    <div className="w-full cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
-                        Restricted Mode: Off
+                    <div className="w-full flex items-center gap-2 cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
+                        <RiUserSettingsLine />  Restricted Mode: Off
                     </div>
-                    <div className="w-full cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
-                        Location: India
+                    <div className="w-full flex items-center gap-2 cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
+                        <TbCurrentLocationFilled />  Location: India
                     </div>
-                    <div className="w-full cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
-                        Keyboard shortcuts
+                    <div className="w-full flex items-center gap-2 cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
+                        <FaKeyboard /> Keyboard shortcuts
                     </div>
                     <div className="my-2 mx-2 border-t"></div>
-                    <div className="w-full cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
-                        Settings
+                    <div className="w-full flex items-center gap-2 cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
+                        <IoSettingsOutline />  Settings
                     </div>
 
                     <div className="my-2 mx-2 border-t"></div>
-                    
-                    <div className="w-full cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
-                        Help
+
+                    <div className="w-full flex items-center gap-2 cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
+                        <MdOutlineHelpOutline />  Help
                     </div>
-                    <div className="w-full cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
-                        Send Feedback
+                    <div className="w-full flex items-center gap-2 cursor-pointer rounded-xl px-4 py-2 text-left hover:bg-gray-900">
+                        <VscFeedback /> Send Feedback
                     </div>
                 </div>
             </div>
