70963c420a4ab4aae665ea45e9c1c4cedad4e5e8
hds05 <duttsharmahimanshu96@gmail.com>
2026-05-02 22:11:03 +0530

    update Login and Register components for improved error handling and UI consistency

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,10 +2,11 @@ import axios from "axios";
 import React, { useState } from "react";
 import { IoArrowBack } from "react-icons/io5";
 import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
 function Login() {
   const navigate = useNavigate();
-
+  const { login } = useAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -15,35 +16,48 @@ function Login() {
     try {
       const res = await axios.post("http://localhost:3000/login", {
         email,
-        password
-      })
+        password,
+      });
       // const userData = {
       //   email, password
       // };
-      console.log(res)
-      localStorage.setItem("token", res.data.token)
+      console.log(res);
+      // localStorage.setItem("token", res.data.token)
+      // localStorage.setItem("user", JSON.stringify(res.data.user));
+
+      login(res.data.user, res.data.token);
+
       navigate("/");
-    }
-    catch (err) {
-      console.log(err.message)
+    } catch (err) {
+      console.log(err.message);
+      alert(err.response?.data?.message || "Something went wrong!!!");
     }
   };
 
   return (
     <div className="bg-black fixed inset-0 z-50 p-2 flex justify-center items-center">
-
       <div className="bg-white w-[350px] rounded-xl p-6 shadow-lg">
-        <button onClick={() => navigate('/')} className=" hover:bg-gray-300 rounded-full p-2 cursor-pointer">
+        <button
+          onClick={() => navigate("/")}
+          className=" hover:bg-gray-300 rounded-full p-2 cursor-pointer"
+        >
           <IoArrowBack size={22} />
         </button>
+        <div className="flex justify-center my-2">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/960px-YouTube_full-color_icon_%282017%29.svg.png"
+            className="w-[100px]"
+            alt=""
+          />
+        </div>
         <div className="text-center mb-4">
           <h1 className="text-2xl font-semibold">Sign in</h1>
           <p className="text-sm text-gray-500">to continue to YouTube</p>
         </div>
 
         <form onSubmit={handleLogin} className="flex flex-col gap-4">
-          <div className='flex flex-col'>
-            <label className='text-sm text-gray-500'>Email</label>
+          <div className="flex flex-col">
+            <label className="text-sm text-gray-500">Email</label>
             <input
               type="email"
               placeholder="Email"
@@ -54,8 +68,8 @@ function Login() {
             />
           </div>
 
-          <div className='flex flex-col'>
-            <label className='text-sm text-gray-500'>Password</label>
+          <div className="flex flex-col">
+            <label className="text-sm text-gray-500">Password</label>
             <input
               type="password"
               placeholder="Password"
@@ -65,25 +79,30 @@ function Login() {
               required
             />
           </div>
-          <button type="submit" className="bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700" >
+          <button
+            type="submit"
+            className="bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700"
+          >
             Sign in
           </button>
         </form>
-        <Link to={'/register'} className="flex justify-end text-blue-500 hover:underline text-xs mt-2">
+        <Link
+          to={"/register"}
+          className="flex justify-end text-blue-500 hover:underline text-xs mt-2"
+        >
           Create account
         </Link>
         <div className="text-center text-sm text-gray-400 my-4">or</div>
 
         <button className="group border w-full py-2 rounded-xl hover:bg-gray-300 hover:shadow-[0px_3px_5px_black] transition cursor-pointer">
           Continue with_
-          <span className='group-hover:text-blue-500 font-bold'>G</span>
-          <span className='group-hover:text-red-500 font-bold'>o</span>
-          <span className='group-hover:text-yellow-500 font-bold'>o</span>
-          <span className='group-hover:text-blue-500 font-bold'>g</span>
-          <span className='group-hover:text-green-500 font-bold'>l</span>
-          <span className='group-hover:text-red-500 font-bold'>e</span>
+          <span className="group-hover:text-blue-500 font-bold">G</span>
+          <span className="group-hover:text-red-500 font-bold">o</span>
+          <span className="group-hover:text-yellow-500 font-bold">o</span>
+          <span className="group-hover:text-blue-500 font-bold">g</span>
+          <span className="group-hover:text-green-500 font-bold">l</span>
+          <span className="group-hover:text-red-500 font-bold">e</span>
         </button>
-
       </div>
     </div>
   );
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,16 +1,16 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-import { IoArrowBack } from 'react-icons/io5'
-import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios";
+import React, { useState } from "react";
+import { IoArrowBack } from "react-icons/io5";
+import { Link, useNavigate } from "react-router-dom";
 
 function Register() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const navigate = useNavigate()
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleRegister = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     // const userData = {
     //   name, email, password
     // }
@@ -18,31 +18,40 @@ function Register() {
       const res = await axios.post(
         "http://localhost:3000/register",
         { name, email, password },
-        { withCredentials: true }
+        { withCredentials: true },
       );
       console.log(res.data);
 
-      navigate('/login')
+      navigate("/login");
     } catch (err) {
       console.log(err.response?.data || err.message);
       alert(err.response?.data?.message || "Something went wrong");
     }
-  }
+  };
   return (
     <div className="bg-black fixed inset-0 z-50 p-2 flex justify-center items-center">
-
       <div className="bg-white w-[350px] rounded-xl p-6 shadow-lg">
-        <button onClick={() => navigate('/')} className=" hover:bg-gray-300 rounded-full p-2 cursor-pointer">
+        <button
+          onClick={() => navigate("/")}
+          className=" hover:bg-gray-300 rounded-full p-2 cursor-pointer"
+        >
           <IoArrowBack size={22} />
         </button>
         <div className="text-center mb-4">
-          <h1 className="text-2xl font-semibold">Sign Up</h1>
+          <div className="flex gap-2 justify-center items-center">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/1020/1020351.png"
+              alt=""
+              className="w-10"
+            />
+            <h1 className="text-2xl font-semibold">Sign Up</h1>
+          </div>
           <p className="text-sm text-gray-500">to continue to YouTube</p>
         </div>
 
         <form onSubmit={handleRegister} className="flex flex-col gap-4">
-          <div className='flex flex-col'>
-            <label className='text-sm text-gray-500'>Username</label>
+          <div className="flex flex-col">
+            <label className="text-sm text-gray-500">Username</label>
             <input
               type="text"
               placeholder="Your name"
@@ -53,8 +62,8 @@ function Register() {
             />
           </div>
 
-          <div className='flex flex-col'>
-            <label className='text-sm text-gray-500'>Email</label>
+          <div className="flex flex-col">
+            <label className="text-sm text-gray-500">Email</label>
             <input
               type="email"
               placeholder="Email"
@@ -65,8 +74,8 @@ function Register() {
             />
           </div>
 
-          <div className='flex flex-col'>
-            <label className='text-sm text-gray-500'>Password</label>
+          <div className="flex flex-col">
+            <label className="text-sm text-gray-500">Password</label>
             <input
               type="password"
               placeholder="Password"
@@ -77,27 +86,33 @@ function Register() {
             />
           </div>
 
-          <button type="submit" className="bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700" >
+          <button
+            type="submit"
+            className="bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700"
+          >
             Sign up
           </button>
         </form>
-        <Link to={'/login'} className="flex justify-end text-blue-500 text-xs mt-2 hover:underline">
+        <Link
+          to={"/login"}
+          className="flex justify-end text-blue-500 text-xs mt-2 hover:underline"
+        >
           Login
         </Link>
         <div className="text-center text-sm text-gray-400 my-4">or</div>
 
         <button className="group border w-full py-2 rounded-xl hover:bg-gray-300 hover:shadow-[0px_3px_5px_black] transition cursor-pointer">
           Continue with_
-          <span className='group-hover:text-blue-500 font-bold'>G</span>
-          <span className='group-hover:text-red-500 font-bold'>o</span>
-          <span className='group-hover:text-yellow-500 font-bold'>o</span>
-          <span className='group-hover:text-blue-500 font-bold'>g</span>
-          <span className='group-hover:text-green-500 font-bold'>l</span>
-          <span className='group-hover:text-red-500 font-bold'>e</span>
+          <span className="group-hover:text-blue-500 font-bold">G</span>
+          <span className="group-hover:text-red-500 font-bold">o</span>
+          <span className="group-hover:text-yellow-500 font-bold">o</span>
+          <span className="group-hover:text-blue-500 font-bold">g</span>
+          <span className="group-hover:text-green-500 font-bold">l</span>
+          <span className="group-hover:text-red-500 font-bold">e</span>
         </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
