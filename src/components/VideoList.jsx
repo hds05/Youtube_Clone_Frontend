215e7bc70c09b0decb95321b08ee2239d4d843ee
hds05 <duttsharmahimanshu96@gmail.com>
2026-05-02 22:11:57 +0530

    update VideoList component to implement loading state and error handling

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -2,15 +2,26 @@ import React, { useEffect, useState } from 'react'
 import VideoCard from './VideoCard'
 import axios from 'axios';
 import { useSearch } from '../../context/SearchContext';
+import Loader from './Loader';
 
 function VideoList({ category }) {
     const [videos, setVideos] = useState([])
     const { searchText } = useSearch()
-
+    const [loading, setLoading] = useState(false)
     useEffect(() => {
-        axios.get("http://localhost:3000/videos")
-            .then(res => setVideos(res.data))
-            .catch(err => console.log(err))
+        const data = async () => {
+            try {
+                setLoading(true)
+                const res = await axios.get("http://localhost:3000/videos")
+                setVideos(res.data)
+            } catch (err) {
+                console.log(err.message);
+                alert(err.message)
+            } finally {
+                setLoading(false)
+            }
+        }
+        data()
     }, []);
     const filteredVideos = videos.filter((video) => {
         const matchesCategory =
@@ -23,13 +34,19 @@ function VideoList({ category }) {
         return matchesCategory && matchesSearch
     })
     return (
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4'>
+        <>
             {
-                filteredVideos.map((e) => (
-                    <VideoCard key={e.videoId} data={e} />
-                ))
+                loading ? <div><Loader /></div>
+                    :
+                    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4'>
+                        {
+                            filteredVideos.map((e) => (
+                                <VideoCard key={e.videoId} data={e} />
+                            ))
+                        }
+                    </div>
             }
-        </div>
+        </>
     )
 }
 
