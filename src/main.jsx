33789687a533ae17eabe96fa7c55a77129706295
hds05 <duttsharmahimanshu96@gmail.com>
2026-05-02 16:14:22 +0530

    update main.jsx to implement lazy loading for components and add error handling with Loader

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,80 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import HomePage from './Pages/HomePage.jsx'
-import Login from './Pages/Login.jsx'
-import Register from './Pages/Register.jsx'
-import VideoPlayer from './Pages/VideoPlayer.jsx'
-import { AuthProvider } from '../context/AuthContext.jsx'
-import { SearchProvider } from '../context/SearchContext.jsx'
+import { lazy, StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AuthProvider } from "../context/AuthContext.jsx";
+import { SearchProvider } from "../context/SearchContext.jsx";
+import Loader from "./components/Loader.jsx";
+
+const HomePage = lazy(() => import("./Pages/HomePage.jsx"));
+const Login = lazy(() => import("./Pages/Login.jsx"));
+const Register = lazy(() => import("./Pages/Register.jsx"));
+const VideoPlayer = lazy(() => import("./Pages/VideoPlayer.jsx"));
+const ChannelPage = lazy(() => import("./Pages/ChannelPage.jsx"));
+const CreateChannel = lazy(() => import("./Pages/CreateChannel.jsx"));
+const NotFoundPage = lazy(() => import("./Pages/NotFoundPage.jsx"));
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: (<Suspense fallback={<Loader />}>
+      <NotFoundPage />
+    </Suspense>),
     children: [
       {
-        path: '/',
-        element: <HomePage />
+        path: "/",
+        element: (
+          <Suspense fallback={<Loader />}>
+            <HomePage />
+          </Suspense>
+        ),
       },
       {
-        path: '/login',
-        element: <Login />
+        path: "/login",
+        element: (
+          <Suspense fallback={<Loader />}>
+            <Login />
+          </Suspense>
+        ),
       },
       {
-        path: '/register',
-        element: <Register />
+        path: "/register",
+        element: (
+          <Suspense fallback={<Loader />}>
+            <Register />
+          </Suspense>
+        ),
       },
       {
-        path: '/video/:id',
-        element: <VideoPlayer />
-      }
-    ]
-  }
-])
-createRoot(document.getElementById('root')).render(
+        path: "/video/:id",
+        element: (
+          <Suspense fallback={<Loader />}>
+            <VideoPlayer />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/channel",
+        element: (
+          <Suspense fallback={<Loader />}>
+            <ChannelPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/createChannel",
+        element: (
+          <Suspense fallback={<Loader />}>
+            <CreateChannel />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <SearchProvider>
@@ -42,4 +82,4 @@ createRoot(document.getElementById('root')).render(
       </SearchProvider>
     </AuthProvider>
   </StrictMode>,
-)
+);
