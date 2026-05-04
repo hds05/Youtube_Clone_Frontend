7ac8ff2f7653be30a554ac2fb7b3df0a7b1fb16e
hds05 <duttsharmahimanshu96@gmail.com>
2026-05-04 07:02:31 +0530

    update VideoCard and VideoList components for improved structure and styling

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,8 +5,8 @@ function VideoCard({data}) {
     const navigate = useNavigate()
     return (
         <div onClick={() => navigate(`/video/${data.videoId}`)} className='text-white hover:bg-gray-800 p-2 rounded-2xl cursor-pointer'>
-            <div>
-                <img src={data.thumbnailUrl} alt="" className='w-full rounded-2xl' />
+            <div className='h-[150px] '>
+                <img src={data.thumbnailUrl} alt="" className='w-full h-full rounded-2xl' />
             </div>
             <h1 className='text-[15px] font-bold'>{data.title}</h1>
             <h2 className='text-xs'>{data.channelId}</h2>
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,53 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import VideoCard from './VideoCard'
-import axios from 'axios';
-import { useSearch } from '../../context/SearchContext';
-import Loader from './Loader';
+import React, { useEffect, useState } from "react";
+import VideoCard from "./VideoCard";
+import axios from "axios";
+import { useSearch } from "../../context/SearchContext";
+import Loader from "./Loader";
 
 function VideoList({ category }) {
-    const [videos, setVideos] = useState([])
-    const { searchText } = useSearch()
-    const [loading, setLoading] = useState(false)
-    useEffect(() => {
-        const data = async () => {
-            try {
-                setLoading(true)
-                const res = await axios.get("http://localhost:3000/videos")
-                setVideos(res.data)
-            } catch (err) {
-                console.log(err.message);
-                alert(err.message)
-            } finally {
-                setLoading(false)
-            }
-        }
-        data()
-    }, []);
-    const filteredVideos = videos.filter((video) => {
-        const matchesCategory =
-            category === "All" ||
-            video.category?.toLowerCase() === category.toLowerCase()
+  const [videos, setVideos] = useState([]);
+  const { searchText } = useSearch();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const data = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get("http://localhost:3000/videos");
+        setVideos(res.data);
+      } catch (err) {
+        console.log(err.message);
+        alert(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    data();
+  }, []);
+  const filteredVideos = videos.filter((video) => {
+    const matchesCategory =
+      category === "All" ||
+      video.category?.toLowerCase() === category.toLowerCase();
 
-        const matchesSearch =
-            video.title?.toLowerCase().includes(searchText.toLowerCase())
+    const matchesSearch = video.title
+      ?.toLowerCase()
+      .includes(searchText.toLowerCase());
 
-        return matchesCategory && matchesSearch
-    })
-    return (
-        <>
-            {
-                loading ? <div><Loader /></div>
-                    :
-                    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4'>
-                        {
-                            filteredVideos.map((e) => (
-                                <VideoCard key={e.videoId} data={e} />
-                            ))
-                        }
-                    </div>
-            }
-        </>
-    )
+    return matchesCategory && matchesSearch;
+  });
+  return (
+    <>
+      {loading ? (
+        <div>
+          <Loader />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
+          {filteredVideos.map((e) => (
+            <VideoCard key={e.videoId} data={e} />
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
 
-export default VideoList
+export default VideoList;
